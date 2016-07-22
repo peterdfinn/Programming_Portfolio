1dea29798270a241384e880bcad899c8097077b7
Peter Finn <peterdfinn@icloud.com>
2016-07-21 20:00:48 -0400

    wrote Codefights/BinaryPalindrome.java

---
+++ Codefights/BinaryPalindrome.java
@@ -0,0 +1,23 @@
+public class BinaryPalindrome {
+    /*
+     * Function: binaryPalindrome
+     *
+     * Description: This function finds the nth palindromic b-bit binary number.
+     *
+     * Inputs:
+     *  b - The length, in bits, of the bit string.
+     *  n - The index of the palindrome to find.
+     *
+     * Return value: The nth number that is a palindrome when expressed as a
+     *  b-bit binary number.
+     */
+    public static int binaryPalindrome(int b, int n) {
+	int leftHalf = (n - 1) << (b / 2);
+	int rightHalf = 0;
+	for (int i = b - 1; i >= b - (b / 2); --i) {
+	    int ithBit = (leftHalf >> i) & 1;
+	    rightHalf += ithBit << ((b - 1) - i);
+	}
+	return leftHalf | rightHalf;
+    }
+}
